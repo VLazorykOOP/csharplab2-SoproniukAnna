e3c88ffb25a600e9ae97afc7421d59005b2a9e0e
SoproniukAnna <124136300+SoproniukAnna@users.noreply.github.com>
2024-02-08 18:32:15 +0200

    Task 3

--- Lab2CSharp/Program.cs
+++ Lab2CSharp/Program.cs
@@ -7,12 +7,17 @@ namespace Lab2CSharp
         static void Main(string[] args)
         {
             Console.WriteLine("          Lab 2 CSharp");
+
             Console.WriteLine("          Task 1 Var 1");
-            Task1_Var1();
+            //Task1_Var1();
             Console.WriteLine("          Task 1 Var 2");
-            Task1_Var2();
+            //Task1_Var2();
+
             Console.WriteLine("          Task 2");
-            Task2();
+            //Task2();
+
+            Console.WriteLine("          Task 3");
+            Task3();
         }
 
         static void Task1_Var1()
@@ -115,6 +120,41 @@ namespace Lab2CSharp
                     k++;
 
             Console.WriteLine($"The number of elements whose value is greater than the value of the previous element: {k}");
+            Console.WriteLine();
+        }
+
+        static void Task3()
+        {
+            int n = 0;
+            do
+            {
+                Console.Write("Input size of array (n): ");
+                n = Convert.ToInt32(Console.ReadLine());
+            } while (n <= 0);
+
+            double[,] arr = new double[n, n];
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"Input [{i}],[{j}] element of array: ");
+                    arr[i, j] = Convert.ToDouble(Console.ReadLine());
+                }
+
+            Console.WriteLine("Your array:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    Console.Write(arr[i, j] + "             ");
+                Console.WriteLine();
+            }
+
+            double diagonalSum = 0;
+            for (int i = 0; i < n; i++)
+                diagonalSum += arr[i, n - 1 - i];
+
+            Console.WriteLine($"Sum of elements on the secondary diagonal: {diagonalSum}");
+            Console.WriteLine();
         }
     }
 }
